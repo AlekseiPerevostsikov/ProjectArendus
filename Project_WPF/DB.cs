8ed762768c0feabc5a62e4b54fff18acc1dc793e
AlekseiPerevostsikov <jieksik05@gmail.com>
2018-05-13 20:19:54 +0300

    DB add Select, Insert, Update, Delete

--- Project_WPF/DB.cs
+++ Project_WPF/DB.cs
@@ -12,10 +12,14 @@ namespace Project_WPF
         static Context c = new Context();
 
 
-        public static List<Kategooria> GetAllCategory()
-        {
-            return c.Kategoorias.ToList();
-        }
+
+
+        /// 
+        ///         Select
+        /// 
+        ///
+
+
 
         public static List<Alamkategooria> GetAllSubCategory()
         {
@@ -23,6 +27,562 @@ namespace Project_WPF
         }
 
 
+        public static List<Arve> GetAllCkecks()
+        {
+            return c.Arves.ToList();
+        }
 
+
+        public static List<Kategooria> GetAllCategorys()
+        {
+            return c.Kategoorias.ToList();
+        }
+
+
+        public static List<Klient> GetAllKlients()
+        {
+            return c.Klients.ToList();
+        }
+
+
+        public static List<Ostukorvi> GetAllBaskets()
+        {
+            return c.Ostukorvis.ToList();
+        }
+
+
+        public static List<Pakkuja> GetAllProviders()
+        {
+            return c.Pakkujas.ToList();
+        }
+
+
+        public static List<SisseTuleb> GetAllArrivedProducts()
+        {
+            return c.SisseTulebs.ToList();
+        }
+
+
+        public static List<SisseTulebArv> GetAllArrivedProductChecks()
+        {
+            return c.SisseTulebArvs.ToList();
+        }
+
+
+        public static List<Toode> GetAllProducts()
+        {
+            return c.Toodes.ToList();
+        }
+
+
+
+
+
+
+        /// 
+        ///         Insert
+        /// 
+        ///
+
+
+
+        public static int AddSubCategory(Alamkategooria subCategory)
+        {
+            int error = 0;
+            try
+            {
+                c.Alamkategoorias.Add(subCategory);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int AddCkeck(Arve check)
+        {
+            int error = 0;
+            try
+            {
+                c.Arves.Add(check);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int AddCategory(Kategooria category)
+        {
+            int error = 0;
+            try
+            {
+                c.Kategoorias.Add(category);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int AddKlient(Klient klient)
+        {
+            int error = 0;
+            try
+            {
+                c.Klients.Add(klient);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int AddBasket(Ostukorvi basket)
+        {
+            int error = 0;
+            try
+            {
+                c.Ostukorvis.Add(basket);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int AddProvider(Pakkuja provider)
+        {
+            int error = 0;
+            try
+            {
+                c.Pakkujas.Add(provider);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int AddArrivedProduct(SisseTuleb arrivedProduct)
+        {
+            int error = 0;
+            try
+            {
+                c.SisseTulebs.Add(arrivedProduct);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int AddArrivedProductCheck(SisseTulebArv arrivedProductCheck)
+        {
+            int error = 0;
+            try
+            {
+                c.SisseTulebArvs.Add(arrivedProductCheck);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int AddProduct(Toode product)
+        {
+            int error = 0;
+            try
+            {
+                c.Toodes.Add(product);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+
+
+        /// 
+        ///         Update
+        /// 
+        ///
+
+
+
+        public static int UpdateSubCategory(Alamkategooria subCategory)
+        {
+            int error = 0;
+            try
+            {
+                var original = c.Alamkategoorias.Find(subCategory.ID);
+                c.Entry(original).CurrentValues.SetValues(subCategory);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int UpdateCkeck(Arve check)
+        {
+            int error = 0;
+            try
+            {
+                var original = c.Alamkategoorias.Find(check.ID);
+                c.Entry(original).CurrentValues.SetValues(check);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int UpdateCategory(Kategooria category)
+        {
+            int error = 0;
+            try
+            {
+                var original = c.Alamkategoorias.Find(category.ID);
+                c.Entry(original).CurrentValues.SetValues(category);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int UpdateKlient(Klient klient)
+        {
+            int error = 0;
+            try
+            {
+                var original = c.Alamkategoorias.Find(klient.ID);
+                c.Entry(original).CurrentValues.SetValues(klient);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int UpdateBasket(Ostukorvi basket)
+        {
+            int error = 0;
+            try
+            {
+                var original = c.Alamkategoorias.Find(basket.ID);
+                c.Entry(original).CurrentValues.SetValues(basket);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int UpdateProvider(Pakkuja provider)
+        {
+            int error = 0;
+            try
+            {
+                var original = c.Alamkategoorias.Find(provider.ID);
+                c.Entry(original).CurrentValues.SetValues(provider);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int UpdateArrivedProduct(SisseTuleb arrivedProduct)
+        {
+            int error = 0;
+            try
+            {
+                var original = c.Alamkategoorias.Find(arrivedProduct.ID);
+                c.Entry(original).CurrentValues.SetValues(arrivedProduct);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int UpdateArrivedProductCheck(SisseTulebArv arrivedProductCheck)
+        {
+            int error = 0;
+            try
+            {
+                var original = c.Alamkategoorias.Find(arrivedProductCheck.ID);
+                c.Entry(original).CurrentValues.SetValues(arrivedProductCheck);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int UpdateProduct(Toode product)
+        {
+            int error = 0;
+            try
+            {
+                var original = c.Alamkategoorias.Find(product.ID);
+                c.Entry(original).CurrentValues.SetValues(product);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+
+
+
+
+        /// 
+        ///         Delete
+        /// 
+        ///
+
+
+
+        public static int DeleteSubCategory(Alamkategooria subCategory)
+        {
+            int error = 0;
+            try
+            {
+                c.Alamkategoorias.Remove(subCategory);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int DeleteCkeck(Arve check)
+        {
+            int error = 0;
+            try
+            {
+                c.Arves.Remove(check);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int DeleteCategory(Kategooria category)
+        {
+            int error = 0;
+            try
+            {
+                c.Kategoorias.Remove(category);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int DeleteKlient(Klient klient)
+        {
+            int error = 0;
+            try
+            {
+                c.Klients.Remove(klient);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int DeleteBasket(Ostukorvi basket)
+        {
+            int error = 0;
+            try
+            {
+                c.Ostukorvis.Remove(basket);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int DeleteProvider(Pakkuja provider)
+        {
+            int error = 0;
+            try
+            {
+                c.Pakkujas.Remove(provider);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int DeleteArrivedProduct(SisseTuleb arrivedProduct)
+        {
+            int error = 0;
+            try
+            {
+                c.SisseTulebs.Remove(arrivedProduct);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int DeleteArrivedProductCheck(SisseTulebArv arrivedProductCheck)
+        {
+            int error = 0;
+            try
+            {
+                c.SisseTulebArvs.Remove(arrivedProductCheck);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+        public static int DeleteProduct(Toode product)
+        {
+            int error = 0;
+            try
+            {
+                c.Toodes.Remove(product);
+                c.SaveChanges();
+                error = 1;
+            }
+            catch
+            {
+                error = 0;
+            }
+            return error;
+        }
+
+
+
+
+
+
+
+
+        public static void Save()
+        {
+            c.SaveChanges();
+        }
     }
 }
