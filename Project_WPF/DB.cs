23c84c60fb1741216c655b67221fd6c20d3aa4ff
AlekseiPerevostsikov <jieksik05@gmail.com>
2018-05-13 19:26:39 +0300

    Add DB

---
+++ Project_WPF/DB.cs
@@ -0,0 +1,28 @@
+﻿using LaduDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_WPF
+{
+    class DB
+    {
+        static Context c = new Context();
+
+
+        public static List<Kategooria> GetAllCategory()
+        {
+            return c.Kategoorias.ToList();
+        }
+
+        public static List<Alamkategooria> GetAllSubCategory()
+        {
+            return c.Alamkategoorias.ToList();
+        }
+
+
+
+    }
+}
